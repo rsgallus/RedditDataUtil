0dd1d03ff0c495551e369205c5dc5d093db1ea91
Ryan Gallus <rsgallus@iastate.edu>
2017-11-16 11:10:56 -0600

    Huge changes, completely redesigned. Changed to list of list of strings and now writes to csv.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# RedditBot
-A simple reddit bot that follows the new section of AskReddit and predicts which posts will be successful.
+# RedditDataUtil
+A simple reddit bot that follows a given subreddit, tracks changes in score, and outputs JSON data.
 
 Note that this is a work in progress.
 
 
 ## Description
-This project uses the Python Reddit API Wrapper (PRAW) and some other simple Python libraries to retrieve new posts from the AskReddit section of Reddit. It then tracks changes in the score of each post over time. After sufficient data has been gathered, it predicts how successful the post will be based on the slop of the data points.
+This project uses the Python Reddit API Wrapper (PRAW) and some other simple Python libraries to retrieve new posts from a given section of Reddit. It then tracks changes in the score of each post over time and outputs that data as JSON.
 
 ## Future Development
-As of now, the slope of the data points over time is the only metric being used to estimate the success of each post. For future development, the plan is to use Tensor Flow, an open source computational library, to train a simple machine learning model. The model will then make predictions based on previously gathered data.
+The long term plan is to use this to gather data about how successful posts perform over time. Eventually, this will be put into a "good" data set and used to train a machine learning model. The model will then make predictions about how successful a post will be.
--- main.py
+++ main.py
@@ -1,60 +1,113 @@
 #!/usr/bin/python
 import praw
-import time
 import datetime
-import post
-
+import time
+import calendar
+import csv
 
 def main():
 
-    # Constants
-    update_interval = 10    # Minutes
-    post_limit = 20         # Posts
-
-    reddit = praw.Reddit(
-        client_id='my client id',
-        client_secret='my client secret',
-        user_agent='my user agent')
+    # Config
+    column_headers = True
+    post_limit = 100
 
     # Initialize our bot
-    user_agent = "Analytics Bot 0.1"
-    r = praw.Reddit(user_agent=user_agent)
+    reddit = praw.Reddit(
+        client_id='QiTk8CZr-w7kwQ',
+        client_secret='DyRNXYDdquexl-n3l9cb1Df709Y',
+        user_agent='RedditDataUtil-1.0')
 
     # Select a subreddit
-    subreddit = r.get_subreddit("askreddit")
+    subreddit = reddit.subreddit("askreddit")
 
-    # Initialize array for tracked posts
-    tracked_posts = []
+    # Initialize array for posts
+    data_list = []
 
-    # Begin main execution loop
-    while True:
+    # Append column headers
+    if column_headers:
+        headers = []
+        headers.append("title")
+        headers.append("username")
+        headers.append("serious")
+        headers.append("time_of_day")
+        headers.append("day_of_week")
+        headers.append("upvote_count")
+        headers.append("comment_count")
+        headers.append("comment_upvote_sum")
+        headers.append("elapsed_time")
+        headers.append("title_word_count")
+        data_list.append(headers)
 
-        # Grab any new posts
-        print("\n ---------------------------------- \n Checking for new posts \n")
-        for submission in subreddit.get_new(limit=post_limit):
+    # Collect posts
+    print "Grabbing new posts"
+    row_count = 0
+    for submission in subreddit.rising(limit=post_limit):
 
-            # Create a new post object
-            new_post = post.Post(submission.title, submission.id)
+        # Create new row
+        row = []
 
-            if new_post not in tracked_posts:
-                tracked_posts.append(new_post)
-                print("New Post: ", new_post.title)
+        # Check our progress
+        row_count += 1
+        if row_count % 10 == 0:
+            print row_count, 'posts read'
 
-        # Track score changes
-        minutes = update_interval
-        while minutes > 0:
-            print("\n\n Updating tracked posts")
+        # Find basic post information
+        row.append(submission.title.encode('utf-8').strip())
+        row.append(submission.author.name.encode('utf-8').strip())
 
-            for current_post in tracked_posts:
-                submission = r.get_submission(submission_id=current_post.id)
-                current_post.add_score(submission.score)
-                current_time = time.time();
-                current_post.add_time(datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S'))
+        # Find serious tag
+        if submission.title.partition(' ')[0] == '[Serious]':
+            row.append("yes")
+        else:
+            row.append("no")
 
-            print("\n Minutes remaining = ", minutes)
-            minutes -= 1
+        # Find time of day
+        current_hour = datetime.datetime.now().hour
+        if current_hour < 4:                            # midnight - 4am
+            row.append("night")
+        elif current_hour > 4 and current_hour < 8:     # 4am - 8am
+            row.append("early_morning")
+        elif current_hour > 8 and current_hour < 12:    # 8am - noon
+            row.append("morning")
+        elif current_hour > 12 and current_hour < 16:    # noon - 4pm
+            row.append("afternoon")
+        elif current_hour > 16 and current_hour < 20:    # 4pm - 8pm
+            row.append("early_evening")
+        elif current_hour > 20 and current_hour < 24:    # 8pm - midnight
+            row.append("evening")
 
-            time.sleep(60)  # Wait 60 seconds
+        # Find day of week
+        row.append(calendar.day_name[datetime.datetime.now().weekday()].lower())
+
+        # Find upvote count
+        row.append(submission.score)
+
+        # Find comment count
+        row.append(submission.num_comments)
+
+        # Find top-level comment score sum
+        submission.comments.replace_more(limit=0)
+        score_sum = 0
+        for comment in submission.comments:
+            score_sum += comment.score
+        row.append(score_sum)
+
+        # Find elapsed time
+        posted_time = datetime.datetime.fromtimestamp(submission.created_utc)
+        elapsed_time = (datetime.datetime.now() - posted_time).total_seconds() / 60.0
+        row.append(int(round(elapsed_time)))
+
+        # Find title word count
+        row.append(submission.title.count(' ') + 1)
+
+        # Add row to data list
+        data_list.append(row)
+
+    # Write to csv file
+    with open("output.csv", "wb") as outputfile:
+        print 'Writing to csv'
+        csvwriter = csv.writer(outputfile)
+        csvwriter.writerows(data_list)
 
 
 if __name__ == '__main__':
